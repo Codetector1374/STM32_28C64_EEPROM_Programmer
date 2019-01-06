1fe11f6cf5fe8de8eb52f99e88204e97d4e5ee96
codetector <codetector@codetector.cn>
2019-01-06 18:20:54 -0500

    74HC165 Wrapper

---
+++ Inc/74hc165.h
@@ -0,0 +1,27 @@
+//
+// Created by codetector on 19-1-6.
+//
+
+#ifndef INC_28C64PROGRAMMER_74HC165_H
+#define INC_28C64PROGRAMMER_74HC165_H
+
+#include "stm32f1xx_hal.h"
+
+typedef struct sr165 {
+    GPIO_TypeDef *ldGPIO;
+    uint16_t ldPin;
+
+    GPIO_TypeDef* clkGPIO;
+    uint16_t clkPin;
+
+    GPIO_TypeDef* dataGPIO;
+    uint16_t dataPin;
+} sr165;
+
+sr165* sr165_create(GPIO_TypeDef* ldGPIO, uint16_t ldPin,
+        GPIO_TypeDef* clkGPIO, uint16_t clkPin,
+        GPIO_TypeDef* dataGPIO, uint16_t dataPin);
+void registerLoad(sr165* sr);
+uint8_t read_one_byte(sr165* sr);
+
+#endif //INC_28C64PROGRAMMER_74HC165_H
---
+++ Src/74hc165.c
@@ -0,0 +1,39 @@
+//
+// Created by codetector on 19-1-6.
+//
+#include "74hc165.h"
+#include "dwt_stm32_delay.h"
+#include <stdlib.h>
+
+sr165* sr165_create(GPIO_TypeDef* ldGPIO, uint16_t ldPin,
+                    GPIO_TypeDef* clkGPIO, uint16_t clkPin,
+                    GPIO_TypeDef* dataGPIO, uint16_t dataPin) {
+    sr165* sr = calloc(sizeof(sr), 1);
+    if (sr == NULL) {
+        return NULL;
+    }
+    sr->ldGPIO = ldGPIO;
+    sr->ldPin = ldPin;
+
+    sr->clkGPIO = clkGPIO;
+    sr->clkPin = clkPin;
+
+    sr->dataGPIO = dataGPIO;
+    sr->dataPin = dataPin;
+
+    HAL_GPIO_WritePin(sr->ldGPIO, sr->ldPin, GPIO_PIN_SET);
+
+    return sr;
+}
+void registerLoad(sr165* sr) {
+    HAL_GPIO_WritePin(sr->ldGPIO, sr->ldPin, GPIO_PIN_RESET);
+    DWT_Delay_us(10);
+    HAL_GPIO_WritePin(sr->ldGPIO, sr->ldPin, GPIO_PIN_SET);
+}
+uint8_t read_one_byte(sr165* sr) {
+    uint8_t data = 0U;
+    for (int i = 7; i >= 0; i--) {
+        data |= (uint8_t) HAL_GPIO_ReadPin(sr->dataGPIO, sr->dataPin) << i;
+    }
+    return data;
+}
