37c98385f2fe13e0eaaa57753cd699b968ce7e86
codetector <codetector@codetector.cn>
2019-01-07 08:42:17 -0500

    I can calloc properly

--- Src/74hc165.c
+++ Src/74hc165.c
@@ -8,7 +8,7 @@
 sr165* sr165_create(GPIO_TypeDef* ldGPIO, uint16_t ldPin,
                     GPIO_TypeDef* clkGPIO, uint16_t clkPin,
                     GPIO_TypeDef* dataGPIO, uint16_t dataPin) {
-    sr165* sr = calloc(sizeof(sr), 1);
+    sr165* sr = calloc(sizeof(sr165), 1);
     if (sr == NULL) {
         return NULL;
     }
@@ -28,16 +28,16 @@ sr165* sr165_create(GPIO_TypeDef* ldGPIO, uint16_t ldPin,
 void sr165_load(sr165* sr) {
     HAL_GPIO_WritePin(sr->ldGPIO, sr->ldPin, GPIO_PIN_RESET);
     DWT_Delay_us(10);
-//    HAL_GPIO_WritePin(sr->ldGPIO, sr->ldPin, GPIO_PIN_SET);
+   HAL_GPIO_WritePin(sr->ldGPIO, sr->ldPin, GPIO_PIN_SET);
 }
 uint8_t sr165_read_one_byte(sr165* sr) {
     uint8_t data = 0U;
     for (int i = 7; i >= 0; i--) {
         data |= (uint8_t) (HAL_GPIO_ReadPin(sr->dataGPIO, sr->dataPin) == GPIO_PIN_SET) << i;
-//        HAL_GPIO_WritePin(sr->clkGPIO, sr->clkPin, GPIO_PIN_SET);
-//        DWT_Delay_us(10);
-//        HAL_GPIO_WritePin(sr->clkGPIO, sr->clkPin, GPIO_PIN_RESET);
-//        DWT_Delay_us(1);
+       HAL_GPIO_WritePin(sr->clkGPIO, sr->clkPin, GPIO_PIN_SET);
+       DWT_Delay_us(10);
+       HAL_GPIO_WritePin(sr->clkGPIO, sr->clkPin, GPIO_PIN_RESET);
+       DWT_Delay_us(1);
     }
     return data;
 }
